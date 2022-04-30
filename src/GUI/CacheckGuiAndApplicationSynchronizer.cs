8b1b817cdcb787aeb43e9e188745b754d275cf68
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2022-04-30 11:57:47 +0200

    Package/-s updated to most recent version

--- src/GUI/CacheckGuiAndApplicationSynchronizer.cs
+++ src/GUI/CacheckGuiAndApplicationSynchronizer.cs
@@ -1,9 +1,10 @@
 ﻿using Aspenlaub.Net.GitHub.CSharp.Cacheck.Entities;
+using Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Interfaces.Application;
 using Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.GUI;
 
 namespace Aspenlaub.Net.GitHub.CSharp.Cacheck.GUI {
     public class CacheckGuiAndApplicationSynchronizer : GuiAndApplicationSynchronizerBase<CacheckApplicationModel, CacheckWindow> {
-        public CacheckGuiAndApplicationSynchronizer(CacheckApplicationModel model, CacheckWindow window) : base(model, window) {
+        public CacheckGuiAndApplicationSynchronizer(CacheckApplicationModel model, CacheckWindow window, IApplicationLogger applicationLogger) : base(model, window, applicationLogger) {
         }
     }
 }
