9fb9af486bdad0eb6b09a7ad8b715365300adbca
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2025-12-10 11:56:48 +0100

    Recent fix was incorrect

--- src/CacheckWindow.xaml.cs
+++ src/CacheckWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
@@ -53,20 +54,18 @@ public partial class CacheckWindow : IAsyncDisposable {
     private async Task OnLoadedAsync() {
         await BuildContainerIfNecessaryAsync();
 
-        if (!CacheckApp.IsIntegrationTest) {
-            var postingCollector = Container.Resolve<IPostingCollector>();
-            var allTimePostings = await postingCollector.CollectPostingsAsync(false);
-            if (allTimePostings.AreAllPostingsPreClassified()) {
-                ClassificationsTab.Visibility = Visibility.Hidden;
-                MonthsTab.Visibility = Visibility.Hidden;
-            }
-        }
-
         _CacheckApp = Container.Resolve<CacheckApplication>();
         await _CacheckApp.OnLoadedAsync();
 
-        var guiToAppGate = Container.Resolve<IGuiToApplicationGate>();
-        var handlers = _CacheckApp.Handlers;
+        IPostingCollector postingCollector = Container.Resolve<IPostingCollector>();
+        IList<IPosting> allTimePostings = await postingCollector.CollectPostingsAsync(false);
+        if (allTimePostings.AreAllPostingsPreClassified()) {
+            ClassificationsTab.Visibility = Visibility.Hidden;
+            MonthsTab.Visibility = Visibility.Hidden;
+        }
+
+        IGuiToApplicationGate guiToAppGate = Container.Resolve<IGuiToApplicationGate>();
+        ICacheckHandlers handlers = _CacheckApp.Handlers;
         guiToAppGate.RegisterAsyncDataGridCallback(OverallSums, handlers.OverallSumsHandler.CollectionChangedAsync);
         guiToAppGate.RegisterAsyncDataGridCallback(ClassificationSums, handlers.ClassificationSumsHandler.CollectionChangedAsync);
         guiToAppGate.RegisterAsyncDataGridCallback(ClassificationAverages, handlers.ClassificationAveragesHandler.CollectionChangedAsync);
@@ -89,7 +88,7 @@ public partial class CacheckWindow : IAsyncDisposable {
 
         _TashTimer.CreateAndStartTimer(_CacheckApp.CreateTashTaskHandlingStatus());
 
-        var dataCollector = Container.Resolve<IDataCollector>();
+        IDataCollector dataCollector = Container.Resolve<IDataCollector>();
         await dataCollector.CollectAndShowAsync();
 
         await ExceptionHandler.RunAsync(WindowsApplication.Current, TimeSpan.FromSeconds(2));
@@ -122,21 +121,21 @@ public partial class CacheckWindow : IAsyncDisposable {
     }
 
     private static async Task SaveClassifiedPostingsAsync() {
-        var dataCollector = Container.Resolve<IDataCollector>();
-        var postingCollector = Container.Resolve<IPostingCollector>();
-        var calculator = Container.Resolve<IClassifiedPostingsCalculator>();
-        var allTimePostings = await postingCollector.CollectPostingsAsync(false);
+        IDataCollector dataCollector = Container.Resolve<IDataCollector>();
+        IPostingCollector postingCollector = Container.Resolve<IPostingCollector>();
+        IClassifiedPostingsCalculator calculator = Container.Resolve<IClassifiedPostingsCalculator>();
+        IList<IPosting> allTimePostings = await postingCollector.CollectPostingsAsync(false);
         if (allTimePostings.AreAllPostingsPreClassified()) { return; }
 
         var errorsAndInfos = new ErrorsAndInfos();
-        var postingClassifications = await dataCollector.CollectPostingClassificationsAsync(errorsAndInfos);
+        List<IPostingClassification> postingClassifications = await dataCollector.CollectPostingClassificationsAsync(errorsAndInfos);
         if (errorsAndInfos.AnyErrors()) { return; }
 
-        var classifiedPostings = await calculator.CalculateAndShowClassifiedPostingsAsync(allTimePostings,
-                                                                                          postingClassifications, DateTime.MinValue, 0, "", "");
-        var exporter = Container.Resolve<IClassifiedPostingsExporter>();
-        var folderResolver = Container.Resolve<IFolderResolver>();
-        var exportFileFullName = await PreClassifiedPostingsSettings.ClassifiedPostingsFileFullNameAsync(folderResolver, errorsAndInfos);
+        IList<IClassifiedPosting> classifiedPostings = await calculator.CalculateAndShowClassifiedPostingsAsync(allTimePostings,
+                                                                                                                postingClassifications, DateTime.MinValue, 0, "", "");
+        IClassifiedPostingsExporter exporter = Container.Resolve<IClassifiedPostingsExporter>();
+        IFolderResolver folderResolver = Container.Resolve<IFolderResolver>();
+        string exportFileFullName = await PreClassifiedPostingsSettings.ClassifiedPostingsFileFullNameAsync(folderResolver, errorsAndInfos);
         if (errorsAndInfos.AnyErrors()) { return; }
 
         exporter.ExportClassifiedPostings(exportFileFullName, classifiedPostings);
@@ -145,7 +144,7 @@ public partial class CacheckWindow : IAsyncDisposable {
     private async Task BuildContainerIfNecessaryAsync() {
         if (Container != null) { return; }
 
-        var builder = await new ContainerBuilder().UseCacheckVishizhukelNetAndPeghAsync(this);
+        ContainerBuilder builder = await new ContainerBuilder().UseCacheckVishizhukelNetAndPeghAsync(this);
         Container = builder.Build();
     }
 
@@ -166,20 +165,20 @@ public partial class CacheckWindow : IAsyncDisposable {
         }
 
         var posting = new Posting { Date = postings[0].Date, Amount = postings[0].Amount, Remark = postings[0].Remark };
-        var hasher = Container.Resolve<IPostingHasher>();
-        var hash = hasher.Hash(posting);
+        IPostingHasher hasher = Container.Resolve<IPostingHasher>();
+        string hash = hasher.Hash(posting);
         var changeClassificationWindow = new ChangeClassificationWindow {
             Posting = posting, PostingHash = hash
         };
 
         var errorsAndInfos = new ErrorsAndInfos();
-        var postingClassificationsSecret = await Container.Resolve<ISecretRepository>().GetAsync(new PostingClassificationsSecret(), errorsAndInfos);
+        PostingClassifications postingClassificationsSecret = await Container.Resolve<ISecretRepository>().GetAsync(new PostingClassificationsSecret(), errorsAndInfos);
         if (errorsAndInfos.AnyErrors()) {
             MessageBox.Show("Could find available classifications", Title, MessageBoxButton.OK, MessageBoxImage.Error);
             return;
         }
 
-        var credit = posting.Amount >= 0;
+        bool credit = posting.Amount >= 0;
         var postingClassifications = postingClassificationsSecret
                 .OfType<IPostingClassification>()
                 .Where(c => c.Credit == credit)
@@ -197,7 +196,7 @@ public partial class CacheckWindow : IAsyncDisposable {
             Credit = credit,
             PostingHash = changeClassificationWindow.Hash.Text
         };
-        var source = Container.Resolve<IIndividualPostingClassificationsSource>();
+        IIndividualPostingClassificationsSource source = Container.Resolve<IIndividualPostingClassificationsSource>();
         await source.AddOrUpdateAsync(individualPostingClassification, errorsAndInfos);
         if (errorsAndInfos.AnyErrors()) {
             MessageBox.Show($"Could not set classification to {individualPostingClassification.Classification} for posting hash {individualPostingClassification.PostingHash}",
@@ -215,10 +214,10 @@ public partial class CacheckWindow : IAsyncDisposable {
     }
 
     private async Task OnRefreshMonthlyDetailsButtonClick() {
-        var oldCursor = Cursor;
+        Cursor oldCursor = Cursor;
         Cursor = Cursors.Wait;
         try {
-            var dataCollector = Container.Resolve<IDataCollector>();
+            IDataCollector dataCollector = Container.Resolve<IDataCollector>();
             await dataCollector.CollectAndShowMonthlyDetailsAsync();
         } finally {
             Cursor = oldCursor;
